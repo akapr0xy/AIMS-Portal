c8a7e5ba74f07f9a1c3c6cc7053d9f30aa8d7492
Paul Sherrod <31117207+akapr0xy@users.noreply.github.com>
2018-10-31 09:59:33 -0700

    Initial commit

---
+++ Controllers/UploadFilesController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIMS_Portal.Controllers
+{
+    public class UploadFilesController : Controller
+    {
+        // GET: UploadFiles
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UploadFiles/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UploadFiles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UploadFiles/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UploadFiles/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UploadFiles/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UploadFiles/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UploadFiles/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AIMS_Portal.Models
+{
+    public class LoginViewModel
+    {
+        public string ReturnUrl { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
