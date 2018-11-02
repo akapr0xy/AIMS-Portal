8fd4731a2ecd8e5e9bcf3910cccfcc46a0cb2c23
Paul Sherrod <31117207+akapr0xy@users.noreply.github.com>
2018-11-02 13:37:07 -0700

    Added CryptoHelper Hash Password Method

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CryptoHelper;
+using AIMS_Portal.Helpers;
 
 namespace AIMS_Portal.Data
 {
-    public static class DbInitializer
-    {
-        
-    }
+    //public static class DbInitializer
+    //{
+    //    public static void SeedData(DbContext dataContext)
+    //    {
+    //        SeedUsers(dataContext);
+    //    }
+
+    //    private static void SeedUsers(DbContext dataContext)
+    //    {
+    //        var users = dataContext.Set<User>();
+
+    //        if(users.Any())
+    //        {
+    //            AddAdminUser(dataContext);
+    //            return;
+    //        }
+    //        AddAdminUser(dataContext);
+    //        for(int i = 0; i < 20; i++)
+    //        {
+    //            users.Add(new User
+    //            {
+    //                Email = $"email{i}@aimscompanies.com",
+    //                Password = CryptoHelpers.HashPassword($"password{i}"),
+    //                Company = $"Company{i}",
+    //                Role = Roles.Customer
+    //            });
+    //        }
+    //    }
+    //}
 }
